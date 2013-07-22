c86da68b2e2f0899ea6203e73652660c829664c1
stevepeak <steve@stevepeak.net>
2013-07-22 13:23:33 -0500

    ch version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # under the License.
 from setuptools import setup
 
-version = '1.3.8'
+version = '1.4.0'
 
 setup(name='timestring',
       version=version,
--- timestring/__init__.py
+++ timestring/__init__.py
@@ -1,6 +1,6 @@
 import re
 
-version = '1.3.8'
+version = '1.4.0'
 __version__ = version
 
 from Date import Date

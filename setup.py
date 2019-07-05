72e129885d42e3352780c60f473def6f1f8a6b50
Eli Hooten <elir.hooten@gmail.com>
2019-07-05 10:12:49 -0500

    Update Build targets
    Update for Python 3.6 and 3.7 in CI and pypi classifiers.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-version = '1.6.3'
+version = '1.6.4'
 classifiers = ["Development Status :: 5 - Production/Stable",
                "License :: OSI Approved :: Apache Software License",
                "Programming Language :: Python",
-               "Programming Language :: Python :: 2.6",
                "Programming Language :: Python :: 2.7",
-               "Programming Language :: Python :: 3.2",
                "Programming Language :: Python :: 3.3",
+               "Programming Language :: Python :: 3.4",
+               "Programming Language :: Python :: 3.5",
+               "Programming Language :: Python :: 3.6",
+               "Programming Language :: Python :: 3.7",
                "Programming Language :: Python :: Implementation :: PyPy"]   
 
 setup(name='timestring',
